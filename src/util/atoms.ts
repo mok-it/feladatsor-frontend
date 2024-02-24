6d3b4c1f14c01173e9c9e05bdb8c49ba22444123
KostyalBalint <kostyal.balintnak@gmail.com>
2024-02-24 21:38:00 +0100

    fix: can't load page

--- src/util/atoms.ts
+++ src/util/atoms.ts
@@ -1,10 +1,15 @@
 import { User as TotalUser } from "@/generated/graphql.tsx";
 import { atomWithStorage, createJSONStorage } from "jotai/utils";
 
-const storage = createJSONStorage<User | null | undefined>(() => sessionStorage)
-export const userAtom = atomWithStorage<User | null | undefined>("user", undefined, storage)
+const storage = createJSONStorage<User | null | undefined>(
+  () => sessionStorage,
+);
+export const userAtom = atomWithStorage<User | null | undefined>(
+  "user",
+  null,
+  storage,
+);
 type User = Omit<Omit<TotalUser, "exercises">, "__typename">;
-
 
 const tokenStore = createJSONStorage<string | undefined>(() => sessionStorage);
 export const tokenAtom = atomWithStorage<string | undefined>(
