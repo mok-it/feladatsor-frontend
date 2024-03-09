954422d2b501b1d830b93776a83b28dd26c369e1
rumizz <rumi.zoard@gmail.com>
2024-03-09 16:31:26 +0100

    Fix filter layout

--- src/pages/ExerciseListPage.tsx
+++ src/pages/ExerciseListPage.tsx
@@ -1,81 +1,71 @@
+import { ExerciseAgeGroup } from "@/generated/graphql";
 import {
   Card,
+  CardContent,
   CardHeader,
-  FormControl,
+  Checkbox,
   Grid,
   IconButton,
   InputAdornment,
-  InputLabel,
-  MenuItem,
-  Checkbox,
-  Input,
-  Select,
   Slider,
   Stack,
   TextField,
-  Typography,
 } from "@mui/material";
-import React, { useState } from "react";
-import { IoSearch } from "react-icons/io5";
+import { entries } from "lodash";
+import { useState } from "react";
 import { BiPlus } from "react-icons/bi";
-import { ExerciseAgeGroup } from "@/generated/graphql";
+import { IoSearch } from "react-icons/io5";
 import { useImmer } from "use-immer";
 
 export const ExerciseListPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [ageGroup, setAgeGroup] = useState("");
-  const [value, setValue] = React.useState<number[]>([20, 37]);
   const [difficulties, setDifficulties] = useImmer<{
     [key in ExerciseAgeGroup]: [number, number];
   }>({
-    KOALA: [20, 37],
-    MEDVEBOCS: [20, 37],
-    NAGYMEDVE: [20, 37],
-    KISMEDVE: [20, 37],
-    JEGESMEDVE: [20, 37],
+    KOALA: [0, 2],
+    MEDVEBOCS: [0, 2],
+    NAGYMEDVE: [0, 2],
+    KISMEDVE: [0, 2],
+    JEGESMEDVE: [0, 2],
   });
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader title="Feladatok keresése" />
-          <Grid container m={2}>
-            <Grid item xs={12} md={6}>
-              <Stack gap={2}>
-                {Object.keys(difficulties).map((difficultyName) => {
-                  return (
-                    <DifficultySelector
-                      ageGroup={difficultyName}
-                      difficulty={difficulties.KOALA}
-                      setDifficulty={(value) =>
-                        setDifficulties((draft) => {
-                          draft.KOALA = value;
-                        })
-                      }
-                      onNewRowClick={() => {}}
-                      isLastRow={true}
-                    />
-                  );
-                })}
-                <TextField
-                  onChange={(event) => setSearchTerm(event.target.value)}
-                  label="Keresés"
-                  value={searchTerm}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <IoSearch />
-                      </InputAdornment>
-                    ),
-                  }}
-                  size="small"
-                />
-              </Stack>
-            </Grid>
-          </Grid>
-        </Card>
-      </Grid>
-    </Grid>
+    <Card>
+      <CardHeader title="Feladatok keresése" />
+      <CardContent>
+        <Grid container>
+          {entries(difficulties).map(([difficultyName, [min, max]]) => {
+            return (
+              <DifficultySelector
+                ageGroup={difficultyName}
+                difficulty={[min, max]}
+                setDifficulty={(value) =>
+                  setDifficulties((draft) => {
+                    draft[difficultyName as ExerciseAgeGroup] = value;
+                  })
+                }
+                onNewRowClick={() => {}}
+                isLastRow={true}
+              />
+            );
+          })}
+        </Grid>
+      </CardContent>
+      <CardContent>
+        <TextField
+          onChange={(event) => setSearchTerm(event.target.value)}
+          label="Keresés"
+          value={searchTerm}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IoSearch />
+              </InputAdornment>
+            ),
+          }}
+          size="small"
+        />
+      </CardContent>
+    </Card>
   );
 };
 
@@ -88,42 +78,36 @@ const DifficultySelector = (props: {
   isLastRow: boolean;
 }) => {
   return (
-    <Stack direction="row" gap={2} alignItems="center">
-      {/* <FormControl fullWidth size="small">
-        <InputLabel id="age-group-label">Korcsoport</InputLabel>
-        <Select
-          labelId="age-group-label"
-          value={props.ageGroup}
-          label="Korcsoport"
-          onChange={(event) => props.setAgeGroup(event.target.value)}
-        >
-          <MenuItem value="KOALA">Koala</MenuItem>
-          <MenuItem value="MEDVEBOCS">Medvebocs</MenuItem>
-          <MenuItem value="KISMEDVE">Kismedve</MenuItem>
-          <MenuItem value="NAGYMEDVE">Nagymedve</MenuItem>
-          <MenuItem value="JEGESMEDVE">Jegesmedve</MenuItem>
-        </Select>
-      </FormControl> */}
-
-      <Checkbox></Checkbox>
-      <p>{props.ageGroup}</p>
-      <Typography variant="body1">Nehézség</Typography>
-      <Slider
-        name="Nehézség"
-        sx={{ width: "100%" }}
-        value={props.difficulty}
-        onChange={(_, value) => props.setDifficulty(value as [number, number])}
-        step={1}
-        marks
-        min={0}
-        max={4}
-        valueLabelDisplay="auto"
-      />
-      {props.isLastRow && (
-        <IconButton color="primary" onClick={() => props.onNewRowClick()}>
-          <BiPlus />
-        </IconButton>
-      )}
-    </Stack>
+    <>
+      <Grid item xs={1}>
+        <Checkbox></Checkbox>
+      </Grid>
+      <Grid item xs={2}>
+        <Stack justifyContent={"center"} height={"100%"}>
+          {props.ageGroup}
+        </Stack>
+      </Grid>
+      <Grid item xs={8} px={8} pt={1}>
+        <Slider
+          name="Nehézség"
+          value={props.difficulty}
+          onChange={(_, value) =>
+            props.setDifficulty(value as [number, number])
+          }
+          step={1}
+          marks
+          min={0}
+          max={4}
+          valueLabelDisplay="auto"
+        />
+      </Grid>
+      <Grid item xs={1}>
+        {props.isLastRow && (
+          <IconButton color="primary" onClick={() => props.onNewRowClick()}>
+            <BiPlus />
+          </IconButton>
+        )}
+      </Grid>
+    </>
   );
 };
