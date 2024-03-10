3569982ec80456a26d80c6d105f7a8a795e48faa
KostyalBalint <kostyal.balintnak@gmail.com>
2024-03-10 10:57:58 +0100

    fix: dataTable imports

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -2,22 +2,25 @@
 /* eslint-disable */
 //This file is from a free MUI theme, don't bother changing it to TypeScript
 
-import { useMemo } from 'react';
-import PropTypes from 'prop-types';
+import { useMemo } from "react";
+import PropTypes from "prop-types";
 
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  createTheme,
+  ThemeProvider as MUIThemeProvider,
+} from "@mui/material/styles";
 
 // @ts-ignore
-import { palette } from './palette';
+import { palette } from "./palette";
 // @ts-ignore
-import { shadows } from './shadows';
+import { shadows } from "./shadows";
 // @ts-ignore
-import { overrides } from './overrides';
+import { overrides } from "./overrides";
 // @ts-ignore
-import { typography } from './typography';
+import { typography } from "./typography";
 // @ts-ignore
-import { customShadows } from './custom-shadows';
+import { customShadows } from "./custom-shadows";
 
 // ----------------------------------------------------------------------
 
@@ -31,9 +34,10 @@ export function ThemeProvider({ children }) {
       customShadows: customShadows(),
       shape: { borderRadius: 8 },
     }),
-    []
+    [],
   );
 
+  // @ts-ignore
   const theme = createTheme(memoizedValue);
 
   theme.components = overrides(theme);
