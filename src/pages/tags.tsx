809df3e5edd665fd20fdded566998a573eb6c3f5
rumizz <rumi.zoard@gmail.com>
2024-03-12 18:46:31 +0100

    Update tags.tsx

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,35 +1,44 @@
-import Box from '@mui/material/Box';
-import { FaArrowAltCircleDown } from "react-icons/fa";
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
-import { FaChevronRight as ChevronRightIcon, FaAngleDown as ExpandMoreIcon } from 'react-icons/fa';
-import { Typography, Card } from '@mui/material';
-import { TreeView } from '@mui/x-tree-view/TreeView';
+import { Card, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import { TreeItem, TreeView } from "@mui/x-tree-view/TreeItem";
+import {
+  FaChevronRight as ChevronRightIcon,
+  FaAngleDown as ExpandMoreIcon,
+} from "react-icons/fa";
 
-function createData(
-  name: string,
-  Parent: string,
-) {
+type Tag = {
+  name: string;
+  Parent: string;
+};
+
+function createData(name: string, Parent: string): Tag {
   return { name, Parent };
 }
 
 const initialRows = [
-  createData('Geometria', 'Kombinatorika'),
-  createData('Kombinatorika', ''),
-  createData('Algebra', ''),
-  createData('Matusz', 'Kombinatorika'),
-  createData('Cigány BT', 'Matusz'),
+  createData("Geometria", "Kombinatorika"),
+  createData("Kombinatorika", ""),
+  createData("Algebra", ""),
+  createData("Matusz", "Kombinatorika"),
+  createData("Cigány BT", "Matusz"),
 ];
 
 export const Tags = () => {
   const renderTree = (nodes: any) => (
-    <TreeItem key={nodes.name} nodeId={nodes.name} label={`${nodes.name}${nodes.Parent ? ` (${nodes.Parent})` : ''}`}>
-      {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+    <TreeItem
+      key={nodes.name}
+      nodeId={nodes.name}
+      label={`${nodes.name}${nodes.Parent ? ` (${nodes.Parent})` : ""}`}
+    >
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node: any) => renderTree(node))
+        : null}
     </TreeItem>
   );
 
-  const generateTree = (rows: any[]) => {
+  const generateTree = (rows: Tag[]) => {
     const tree: any = {};
-    rows.forEach(row => {
+    rows.forEach((row: { name: string | number; Parent: string | number }) => {
       if (!tree[row.name]) {
         tree[row.name] = { ...row, children: [] };
       }
@@ -44,12 +53,11 @@ export const Tags = () => {
 
   return (
     <Box mb={16}>
-      <Typography variant="h4" style={{ margin: '16px' }}>
+      <Typography variant="h4" style={{ margin: "16px" }}>
         Címkék
       </Typography>
       <Card>
         <Box p={2}>
-          
           <TreeView
             aria-label="file system navigator"
             defaultCollapseIcon={<ExpandMoreIcon />}
