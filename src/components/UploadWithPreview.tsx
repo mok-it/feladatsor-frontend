d8dcbe2538104a79f76970a3b0689d032e9b5673
KostyalBalint <kostyal.balint@icloud.com>
2024-12-28 16:10:58 +0100

    feat: exercise page wip

--- src/components/UploadWithPreview.tsx
+++ src/components/UploadWithPreview.tsx
@@ -2,23 +2,38 @@ import { AlertDialog } from "@/components/Dialog.tsx";
 import { UploadDialog } from "@/components/UploadDialog.tsx";
 import { toBase64 } from "@/util/toBase64";
 import { useUploadImage } from "@/util/useUploadImage";
-import { Box, Button, Stack } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Stack,
+} from "@mui/material";
 import { useState } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import { useToggle } from "react-use";
+import { grey } from "@mui/material/colors";
+import { IoClose } from "react-icons/io5";
 
 type UploadWithPreviewProps = {
   defaultUrl?: string | null | undefined;
   onChange: (values: { id: string | null; url: string | null }) => void;
+  title?: string;
+  imageMaxWidth?: string;
 };
 
 export const UploadWithPreview = ({
   onChange: onPropChange,
   defaultUrl,
+  title,
+  imageMaxWidth,
 }: UploadWithPreviewProps) => {
   const [url, setUrl] = useState<string | null>(defaultUrl || null);
-  const [dialogOpen, setDialogOpen] = useState(false);
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const [loading, toggleLoading] = useToggle(false);
+
+  const [imagePreviewDialogOpen, setImagePreviewDialogOpen] = useState(false);
 
   const uploadImage = useUploadImage();
   const onChange = async (file: File | null) => {
@@ -54,46 +69,83 @@ export const UploadWithPreview = ({
               aspectRatio: "16/9",
               maxWidth: {
                 xs: "100%",
-                md: "50%",
+                md: imageMaxWidth ?? "50%",
               },
               overflow: "hidden",
+              position: "relative",
+              cursor: "pointer",
             }}
             borderRadius={1}
+            onClick={() => setImagePreviewDialogOpen(true)}
           >
             <img src={url} alt="preview" />
+            <IconButton
+              aria-label="delete"
+              size="medium"
+              sx={{
+                position: "absolute",
+                bottom: "10px",
+                right: "10px",
+                color: (theme) =>
+                  theme.palette.mode === "light" ? "black" : "white",
+                ":hover": {
+                  background: (theme) =>
+                    theme.palette.mode === "light" ? grey[300] : grey[800],
+                },
+                background: (theme) =>
+                  theme.palette.mode === "light" ? "white" : "black",
+              }}
+              onClick={(event) => {
+                event.stopPropagation();
+                setDeleteDialogOpen(true);
+              }}
+            >
+              <MdDeleteOutline />
+            </IconButton>
           </Box>
-          <Button
-            sx={{
-              maxWidth: {
-                xs: "100%",
-                md: "50%",
-              },
-              overflow: "hidden",
-            }}
-            onClick={() => {
-              setDialogOpen(true);
-            }}
-          >
-            <MdDeleteOutline />
-            Törlés
-          </Button>
         </Stack>
       )}
       <AlertDialog
-        open={dialogOpen}
-        description="Biztosan törlöd ezt a képet?"
+        open={deleteDialogOpen}
+        description={`Biztosan törlöd a (${title}) képet?`}
         secondaryClick={() => {
-          setDialogOpen(false);
+          setDeleteDialogOpen(false);
         }}
         handleClose={() => {
-          setDialogOpen(false);
+          setDeleteDialogOpen(false);
         }}
         primaryClick={() => {
           setUrl(null);
           onChange(null);
-          setDialogOpen(false);
+          setDeleteDialogOpen(false);
         }}
       />
+      <Dialog
+        open={imagePreviewDialogOpen}
+        maxWidth="lg"
+        onClose={() => setImagePreviewDialogOpen(false)}
+        sx={{ width: "100%" }}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          {title}
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={() => setImagePreviewDialogOpen(false)}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+        >
+          <IoClose />
+        </IconButton>
+        <DialogContent>
+          {!url && "Image not selected"}
+          <img src={url ?? undefined} alt="preview" />
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
