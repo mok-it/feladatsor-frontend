9bf6b8ada780fb1486233a659fbd073062485b92
Zoárd Rumi <rumi.zoard@gmail.com>
2026-01-10 11:07:55 +0100

    exercise links (#84)

--- src/components/InfiniteLoad/ExerciseRow.tsx
+++ src/components/InfiniteLoad/ExerciseRow.tsx
@@ -13,19 +13,25 @@ import {
   Typography,
   useMediaQuery,
 } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
 import dayjs from "dayjs";
-import { FC, memo } from "react";
-import { useNavigate } from "react-router-dom";
+import { FC, memo, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { CategoryDifficulties } from "../CategoryDifficulties";
 import { Checks } from "../Checks";
 import { KaTeX } from "../Katex";
 
 const ExerciseRow: FC<{
+  isSelected?: boolean;
+  onSelect?: (id: string) => void;
   data: ExerciseListElemFragment;
-  onClick?: (id: string) => void;
-}> = ({ data, onClick }) => {
+}> = ({ isSelected: isSelectedProp, data, onSelect }) => {
+  const [isSelected, setSelected] = useState(isSelectedProp);
+  useEffect(() => {
+    setSelected(isSelectedProp);
+  }, [isSelectedProp]);
+
   const isMobile = useMediaQuery("(max-width: 1200px)");
-  const navigate = useNavigate();
   const difficulties: Record<ExerciseAgeGroup, number> = {
     KOALA: 0,
     MEDVEBOCS: 0,
@@ -39,65 +45,77 @@ const ExerciseRow: FC<{
   });
 
   return (
-    <StyledTableRow
-      onClick={() =>
-        onClick ? onClick(data.id) : navigate(`/exercise/${data.id}`)
-      }
-      sx={{
-        cursor: "pointer",
-      }}
-    >
+    <StyledTableRow>
       {isMobile ? (
-        <TableCell>
-          <Stack spacing={1}>
-            <Stack
-              direction="row"
-              alignItems={"center"}
-              flexWrap={"wrap"}
-              rowGap={1}
-              spacing={1}
-            >
-              <Box sx={{ whiteSpace: "nowrap" }}>{"#" + data.id}</Box>
-              {ExerciseStatusBadge(data)}
-              <Checks data={data.checks} />
-              <Box flexGrow={1} />
-              <div style={{ margin: 0 }}>
-                <CategoryDifficulties value={difficulties} />
-              </div>
-            </Stack>
-            <Stack
-              direction="row"
-              alignItems={"center"}
-              gap={1}
-              flexWrap="wrap"
-            >
-              {data.tags.map((tag, index) => (
-                <Chip size="small" key={index} label={tag.name} />
-              ))}
-            </Stack>
-            <Tooltip title={<KaTeX fixNewLines value={data.description} />}>
-              <Box
-                sx={{
-                  fontSize: 14,
-                  maxHeight: 40,
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  display: "-webkit-box",
-                  WebkitLineClamp: "2",
-                  WebkitBoxOrient: "vertical",
-                }}
+        <Link
+          target="_blank"
+          style={{ textDecoration: "none" }}
+          to={`/exercise/${data.id}`}
+        >
+          <TableCell>
+            <Stack spacing={1}>
+              <Stack
+                direction="row"
+                alignItems={"center"}
+                flexWrap={"wrap"}
+                rowGap={1}
+                spacing={1}
               >
-                <KaTeX value={data.description} />
-              </Box>
-            </Tooltip>
-            <Typography variant="caption">
-              {dayjs(+data.createdAt).format("YYYY. MM. DD.")}
-            </Typography>
-          </Stack>
-        </TableCell>
+                <Box sx={{ whiteSpace: "nowrap" }}>{"#" + data.id}</Box>
+                {ExerciseStatusBadge(data)}
+                <Checks data={data.checks} />
+                <Box flexGrow={1} />
+                <div style={{ margin: 0 }}>
+                  <CategoryDifficulties value={difficulties} />
+                </div>
+              </Stack>
+              <Stack
+                direction="row"
+                alignItems={"center"}
+                gap={1}
+                flexWrap="wrap"
+              >
+                {data.tags.map((tag, index) => (
+                  <Chip size="small" key={index} label={tag.name} />
+                ))}
+              </Stack>
+              <Tooltip title={<KaTeX fixNewLines value={data.description} />}>
+                <Box
+                  sx={{
+                    fontSize: 14,
+                    maxHeight: 40,
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: "2",
+                    WebkitBoxOrient: "vertical",
+                  }}
+                >
+                  <KaTeX value={data.description} />
+                </Box>
+              </Tooltip>
+              <Typography variant="caption">
+                {dayjs(+data.createdAt).format("YYYY. MM. DD.")}
+              </Typography>
+            </Stack>
+          </TableCell>
+        </Link>
       ) : (
         <>
-          <TableCell sx={{ minWidth: 100 }}>{"#" + data.id}</TableCell>
+          <TableCell sx={{ minWidth: 100 }}>
+            {onSelect && (
+              <Checkbox
+                checked={isSelected}
+                onChange={() => {
+                  setSelected(true);
+                  onSelect(data.id);
+                }}
+              />
+            )}
+            <Link target="_blank" to={`/exercise/${data.id}`}>
+              {"#" + data.id}
+            </Link>
+          </TableCell>
           <TableCell sx={{ minWidth: 260 }}>
             <CategoryDifficulties value={difficulties} />
           </TableCell>
